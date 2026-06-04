e782e53cee4fba7df7348e483f1abc48a705c841
alskung1101 <alskung1101@naver.com>
2026-06-04 09:51:47 +0900

    feat: 회사, 부서, 회원 기본 도메인 추가

---
+++ src/main/java/com/ieumsae/assetieum/domain/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.ieumsae.assetieum.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface
+MemberRepository extends JpaRepository<Member, UUID> {
+
+	Optional<Member> findByEmployeeNumber(String employeeNumber);
+}
