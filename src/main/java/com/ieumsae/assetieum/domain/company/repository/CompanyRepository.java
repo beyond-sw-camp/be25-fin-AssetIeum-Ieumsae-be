51511cb630cf18f4cfbb8b5b611f25f92a43cdb2
alskung1101 <alskung1101@naver.com>
2026-06-04 15:32:57 +0900

    feat: 회사 코드 중복 확인 쿼리 추가

--- src/main/java/com/ieumsae/assetieum/domain/company/repository/CompanyRepository.java
+++ src/main/java/com/ieumsae/assetieum/domain/company/repository/CompanyRepository.java
@@ -5,4 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+
+	boolean existsByCompanyCodeAndDeletedAtIsNull(String companyCode);
 }
