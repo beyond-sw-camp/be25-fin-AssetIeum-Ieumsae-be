1d25915a2b4d98eaf99c101a0438ddf0f750c768
nueeaeel <dodms1006@naver.com>
2026-06-10 15:10:23 +0900

    feat: 무형자산 품목 등록 구현

--- src/main/java/com/ieumsae/assetieum/domain/intangibleasset/item/controller/IntangibleAssetItemController.java
+++ src/main/java/com/ieumsae/assetieum/domain/intangibleasset/item/controller/IntangibleAssetItemController.java
@@ -1,15 +1,20 @@
 package com.ieumsae.assetieum.domain.intangibleasset.item.controller;
 
+import com.ieumsae.assetieum.domain.intangibleasset.item.dto.IntangibleAssetItemCreateRequest;
 import com.ieumsae.assetieum.domain.intangibleasset.item.dto.IntangibleAssetItemDeleteResponse;
 import com.ieumsae.assetieum.domain.intangibleasset.item.dto.IntangibleAssetItemResponse;
 import com.ieumsae.assetieum.domain.intangibleasset.item.dto.IntangibleAssetItemUpdateRequest;
 import com.ieumsae.assetieum.domain.intangibleasset.item.service.IntangibleAssetItemService;
+import com.ieumsae.assetieum.global.common.page.PaginationResponse;
 import com.ieumsae.assetieum.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,26 @@ import java.util.UUID;
 @RequestMapping("/api/v1/intangible-asset/items")
 public class IntangibleAssetItemController {
     private final IntangibleAssetItemService intangibleAssetItemService;
+
+    @PostMapping
+    public ApiResponse<IntangibleAssetItemResponse> createItem(
+            @Valid @RequestBody IntangibleAssetItemCreateRequest request
+    ) {
+        IntangibleAssetItemResponse response =
+                intangibleAssetItemService.createItem(request);
+
+        return ApiResponse.ok("무형자산 품목이 등록되었습니다.", response);
+    }
+
+//    @GetMapping
+//    public ApiResponse<PaginationResponse<IntangibleAssetItemResponse>> getItems(
+//            @Valid @ModelAttribute IntangibleAssetItemSearchRequest request
+//    ) {
+//        PaginationResponse<IntangibleAssetItemResponse> response =
+//                intangibleAssetItemService.getItems(request);
+//
+//        return ApiResponse.ok("무형자산 품목 목록 조회에 성공했습니다.", response);
+//    }
 
     @PatchMapping("/{itemId}")
     public ApiResponse<IntangibleAssetItemResponse> updateItem(
