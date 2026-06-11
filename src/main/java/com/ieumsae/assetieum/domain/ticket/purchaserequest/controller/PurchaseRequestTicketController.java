c060bd1b33c570cca5328cc6cfa559c934b1e399
alskung1101 <162026230+alskung1101@users.noreply.github.com>
2026-06-11 12:13:43 +0900

    [feat] 직접 구매 요청 티켓 등록 API 구현 (#66)
    * refactor: 에러코드 수정
    
    * feat: 직접구매 요청 티켓 등록 API 추가
    
    * refactor: 티켓 번호 생성 로직 공통화

--- src/main/java/com/ieumsae/assetieum/domain/ticket/purchaserequest/controller/PurchaseRequestTicketController.java
+++ src/main/java/com/ieumsae/assetieum/domain/ticket/purchaserequest/controller/PurchaseRequestTicketController.java
@@ -1,5 +1,6 @@
 package com.ieumsae.assetieum.domain.ticket.purchaserequest.controller;
 
+import com.ieumsae.assetieum.domain.ticket.purchaserequest.dto.DirectPurchaseRequestTicketCreateRequest;
 import com.ieumsae.assetieum.domain.ticket.purchaserequest.dto.PurchaseRequestTicketCreateRequest;
 import com.ieumsae.assetieum.domain.ticket.purchaserequest.dto.PurchaseRequestTicketCreateResponse;
 import com.ieumsae.assetieum.domain.ticket.purchaserequest.service.PurchaseRequestTicketService;
@@ -25,10 +26,22 @@ public class PurchaseRequestTicketController {
 		@AuthenticationPrincipal AuthenticatedMember authenticatedMember,
 		@Valid @RequestBody PurchaseRequestTicketCreateRequest request
 	) {
-		PurchaseRequestTicketCreateResponse response = purchaseRequestTicketService.createPurchaseRequestTicket(
+		PurchaseRequestTicketCreateResponse response = purchaseRequestTicketService.createTeamPurchaseRequestTicket(
 			authenticatedMember,
 			request
 		);
 		return ApiResponse.ok("구매 요청 티켓 등록에 성공했습니다.", response);
+	}
+
+	@PostMapping("/direct-purchase")
+	public ApiResponse<PurchaseRequestTicketCreateResponse> createDirectPurchaseRequestTicket(
+		@AuthenticationPrincipal AuthenticatedMember authenticatedMember,
+		@Valid @RequestBody DirectPurchaseRequestTicketCreateRequest request
+	) {
+		PurchaseRequestTicketCreateResponse response = purchaseRequestTicketService.createDirectPurchaseRequestTicket(
+			authenticatedMember,
+			request
+		);
+		return ApiResponse.ok("직접 구매 요청 티켓 등록에 성공했습니다.", response);
 	}
 }
