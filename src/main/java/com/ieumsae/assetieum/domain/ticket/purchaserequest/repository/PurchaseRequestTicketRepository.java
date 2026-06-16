e5dea473c595f7334aa4608a0b4a09c9bf27f4d9
nueeaeel <dodms1006@naver.com>
2026-06-16 12:49:50 +0900

    feat: 구매 정책 설정 구현

--- src/main/java/com/ieumsae/assetieum/domain/ticket/purchaserequest/repository/PurchaseRequestTicketRepository.java
+++ src/main/java/com/ieumsae/assetieum/domain/ticket/purchaserequest/repository/PurchaseRequestTicketRepository.java
@@ -1,11 +1,21 @@
 package com.ieumsae.assetieum.domain.ticket.purchaserequest.repository;
 
+import com.ieumsae.assetieum.domain.ticket.common.type.RequestMethod;
+import com.ieumsae.assetieum.domain.ticket.common.type.TicketStatus;
 import com.ieumsae.assetieum.domain.ticket.purchaserequest.entity.PurchaseRequestTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseRequestTicketRepository extends JpaRepository<PurchaseRequestTicket, UUID> {
 
     Optional<PurchaseRequestTicket> findByIdAndCompany_Id(UUID ticketId, UUID companyId);
+
+    boolean existsByCompany_IdAndRequestMethodAndDeletedAtIsNullAndTicket_TicketStatusIn(
+            UUID companyId,
+            RequestMethod requestMethod,
+            Collection<TicketStatus> ticketStatuses
+    );
 }
