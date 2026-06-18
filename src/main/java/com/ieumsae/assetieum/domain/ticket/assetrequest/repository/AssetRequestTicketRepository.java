c659b097b7b1465ac66cb8f54fe6b7189313e277
alskung1101 <alskung1101@naver.com>
2026-06-18 11:06:35 +0900

    feat: 티켓 생성 시 예외 처리 수정

--- src/main/java/com/ieumsae/assetieum/domain/ticket/assetrequest/repository/AssetRequestTicketRepository.java
+++ src/main/java/com/ieumsae/assetieum/domain/ticket/assetrequest/repository/AssetRequestTicketRepository.java
@@ -1,8 +1,11 @@
 package com.ieumsae.assetieum.domain.ticket.assetrequest.repository;
 
 import com.ieumsae.assetieum.domain.ticket.assetrequest.entity.AssetRequestTicket;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssetRequestTicketRepository extends JpaRepository<AssetRequestTicket, UUID> {
+
+	Optional<AssetRequestTicket> findByIdAndCompany_IdAndDeletedAtIsNull(UUID ticketId, UUID companyId);
 }
