607583f34b16d938f94e0c0b8371f75456b86824
nueeaeel <dodms1006@naver.com>
2026-06-22 15:21:36 +0900

    feat: HR 이벤트에 휴직, 복직 삭제 처리 및 HR 이벤트 대상 자산 테이블 추가

--- src/main/java/com/ieumsae/assetieum/domain/hr/hrevent/type/HrEventType.java
+++ src/main/java/com/ieumsae/assetieum/domain/hr/hrevent/type/HrEventType.java
@@ -3,7 +3,5 @@ package com.ieumsae.assetieum.domain.hr.hrevent.type;
 public enum HrEventType {
     ONBOARDING,
     OFFBOARDING,
-    DEPARTMENT_TRANSFER,
-    LEAVE,
-    RETURN
+    DEPARTMENT_TRANSFER
 }
