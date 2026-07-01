5efc5704169a61333e68cdf19706794c4350a2b0
alskung1101 <alskung1101@naver.com>
2026-07-01 17:28:59 +0900

    refactor: 시간 로직 보강

--- src/main/java/com/ieumsae/assetieum/global/config/TimeZoneConfig.java
+++ src/main/java/com/ieumsae/assetieum/global/config/TimeZoneConfig.java
@@ -1,16 +1,27 @@
 package com.ieumsae.assetieum.global.config;
 
 import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
 import java.util.TimeZone;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 
 @Configuration
 public class TimeZoneConfig {
 
 	private static final String DEFAULT_TIME_ZONE = "Asia/Seoul";
+	private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of(DEFAULT_TIME_ZONE);
 
 	@PostConstruct
 	public void setDefaultTimeZone() {
 		TimeZone.setDefault(TimeZone.getTimeZone(DEFAULT_TIME_ZONE));
+	}
+
+	@Bean
+	public DateTimeProvider seoulDateTimeProvider() {
+		return () -> Optional.of(LocalDateTime.now(DEFAULT_ZONE_ID));
 	}
 }
